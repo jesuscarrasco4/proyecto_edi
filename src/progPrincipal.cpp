a7ae8544aeeea1b15ee38480b16cfcb4075d8230
Jesús Carrasco <jesusscarrassco4@gmail.com>
2026-05-08 17:40:12 +0200

    Update progPrincipal.cpp

--- src/progPrincipal.cpp
+++ src/progPrincipal.cpp
@@ -1,97 +1,133 @@
+//============================================================================
+// Name        : progPrincipal.cpp
+// Author      : Profesores de la asignatura EDI
+// Version     : Curso 25/26
+// Copyright   :
+// Description : Programa principal proyecto EDI
+//============================================================================
+
 #include <iostream>
+#include <string>
 #include <limits>
 #include "Sistema.h"
-#include "Pruebas.h"
-
-void pruebasGestorUsuarios();
 
 using namespace std;
 
-void mostrarMenu() {
-    cout << "\n=============   MENU   =============" << endl;
-    cout << "1. Mostrar todos los Usuarios" << endl;
-    cout << "2. Mostrar todos los Artistas y Canciones" << endl;
-    cout << "3. Buscar Usuario" << endl;
-    cout << "4. Compartir PlayList entre Usuarios" << endl;
-    cout << "5. Buscar Artista por nombre" << endl;
-    cout << "6. Eliminar PlayList" << endl;
-    cout << "7. Añadir Artista a Favoritos" << endl;
-    cout << "8. Eliminar Artista de Favoritos" << endl;
-    cout << "9. Mostrar Artista con mas Seguidores" << endl;
-    cout << "10. Ejecutar Bateria de Pruebas" << endl;
-    cout << "0. Salir" << endl;
-    cout << "Seleccione una opcion: ";
+// Menú
+int menu(string nombreSistema) {
+    int opcion;
+    do {
+        cout << endl << endl;
+        cout << " -------- " << nombreSistema << " --------" << endl << endl;
+        cout << " 1. Mostrar todos los usuarios             " << endl;
+        cout << " 2. Buscar y mostrar un usuario            " << endl;
+        cout << " 3. Mostrar artistas y sus canciones       " << endl;
+        cout << " 4. Buscar y mostrar un artista            " << endl;
+        cout << " 5. Reproducir una playlist de un usuario  " << endl;
+        cout << " 6. Compartir playlist entre dos usuarios  " << endl;
+        cout << " 7. Eliminar playlist de un usuario        " << endl;
+        cout << " 8. Añadir artista a favoritos de un usuario" << endl;
+        cout << " 9. Eliminar artista de favoritos de un usuario" << endl;
+        cout << " 10. Artista con mas seguidores          " << endl;
+
+        cout << " 0. Finalizar. " << endl;
+        cout << " Opción: ";
+        cin >> opcion;
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << endl;
+
+    } while ((opcion < 0) || (opcion > 10));
+    return opcion;
 }
 
+// Función principal
 int main() {
     Sistema *miSistema = new Sistema("Sistema EDI");
+
+    bool salir = false;
     int opcion;
+
     string nombreU, nombreDest, nombrePL, nombreArtista;
 
-    do {
-        mostrarMenu();
-        cin >> opcion;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Limpiar el buffer tras leer un entero
+    while (!salir) {
+        opcion = menu("Simulador Spotify");
 
         switch (opcion) {
-            case 1:
-                miSistema->mostrarUsuarios();
-                break;
-            case 2:
-                miSistema->mostrarArtistas();
-                break;
-            case 3:
-                cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
-                getline(cin, nombreU);
-                miSistema->buscarUsuario(nombreU);
-                break;
-            case 4:
-                cout << "Usuario Origen: "; getline(cin, nombreU);
-                cout << "Nombre de la PlayList: "; getline(cin, nombrePL);
-                cout << "Usuario Destino: "; getline(cin, nombreDest);
-                miSistema->compartirPlayList(nombreU, nombrePL, nombreDest);
-                break;
-            case 5:
-                cout << "Introduce el nombre del artista: ";
-                getline(cin, nombreArtista);
-                miSistema->buscarArtista(nombreArtista);
-                break;
-            case 6:
-                cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
-                getline(cin, nombreU);
-                cout << "Nombre de la PlayList: ";
-                getline(cin, nombrePL);
-                miSistema->eliminarPlayList(nombreU, nombrePL);
-                break;
-            case 7:
-                cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
-                getline(cin, nombreU);
-                cout << "Introduce el nombre del artista: ";
-                getline(cin, nombreArtista);
-                miSistema->anadirArtistaFavorito(nombreU, nombreArtista);
-                break;
-            case 8:
-                cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
-                getline(cin, nombreU);
-                cout << "Introduce el nombre del artista: ";
-                getline(cin, nombreArtista);
-                miSistema->eliminarArtistaFavorito(nombreU, nombreArtista);
-                break;
-            case 9:
-                miSistema->mostrarArtistaConMasSeguidores();
-                break;
-            case 10:
-            	cout << "\n[SISTEMA] Iniciando bateria de pruebas..." << endl;
-            	ejecutarPruebas();
-            	break;
-            case 0:
-                cout << "Saliendo del sistema..." << endl;
-                break;
-            default:
-                cout << "Opcion no valida." << endl;
+        case 1:
+            miSistema->mostrarUsuarios();
+            break;
+
+        case 2:
+            cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
+            getline(cin, nombreU);
+            miSistema->buscarUsuario(nombreU);
+            break;
+
+        case 3:
+            miSistema->mostrarArtistas();
+            break;
+
+        case 4:
+            cout << "Introduce el nombre del artista: ";
+            getline(cin, nombreArtista);
+            miSistema->buscarArtista(nombreArtista);
+            break;
+
+        case 5:
+            cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
+            getline(cin, nombreU);
+            cout << "Nombre de la PlayList: ";
+            getline(cin, nombrePL);
+            cout << "[Aviso] Asegúrate de tener el método implementado en Sistema." << endl;
+            break;
+
+        case 6:
+            cout << "Usuario Origen: "; getline(cin, nombreU);
+            cout << "Nombre de la PlayList: "; getline(cin, nombrePL);
+            cout << "Usuario Destino: "; getline(cin, nombreDest);
+            miSistema->compartirPlayList(nombreU, nombrePL, nombreDest);
+            break;
+
+        case 7:
+            cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
+            getline(cin, nombreU);
+            cout << "Nombre de la PlayList: ";
+            getline(cin, nombrePL);
+            miSistema->eliminarPlayList(nombreU, nombrePL);
+            break;
+
+        case 8:
+            cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
+            getline(cin, nombreU);
+            cout << "Introduce el nombre del artista: ";
+            getline(cin, nombreArtista);
+            miSistema->anadirArtistaFavorito(nombreU, nombreArtista);
+            break;
+
+        case 9:
+            cout << "Introduce el nombre del usuario (Apellido, Nombre): ";
+            getline(cin, nombreU);
+            cout << "Introduce el nombre del artista: ";
+            getline(cin, nombreArtista);
+            miSistema->eliminarArtistaFavorito(nombreU, nombreArtista);
+            break;
+
+        case 10:
+            miSistema->mostrarArtistaConMasSeguidores();
+            break;
+
+        case 0:
+            salir = true;
+            cout << "Saliendo del sistema..." << endl;
+            break;
+
+        default:
+            break;
         }
-    } while (opcion != 0);
+    }
 
     delete miSistema;
+
     return 0;
 }
